b68768d8d791e28f8d5bc260a75a2f2bd860589f
Filip Mikus <xmikusf@stuba.sk>
2024-02-04 20:20:54 +0100

    fix: YOLO-NAS deleted

--- api/app/main.py
+++ api/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from api.app.router import (
     data_converter_router, data_explorer_router, rt_detr_router,
-    sahi_router, sam_router, yolo_nas_router, yolo_v8_router
+    sahi_router, sam_router, yolo_v8_router
 )
 
 
@@ -14,5 +14,4 @@ app.include_router(data_explorer_router.data_explorer_router)
 app.include_router(rt_detr_router.rt_detr_router)
 app.include_router(sahi_router.sahi_router)
 app.include_router(sam_router.sam_router)
-app.include_router(yolo_nas_router.yolo_nas_router)
 app.include_router(yolo_v8_router.yolo_v8_router)
