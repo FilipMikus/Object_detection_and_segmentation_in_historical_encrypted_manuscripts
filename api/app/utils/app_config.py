aa592d7a615842c4b8bd3a22c350146b36e02825
Filip Mikus <xmikusf@stuba.sk>
2024-02-19 23:00:57 +0100

    feat: App Config added

--- api/app/utils/app_config.py
+++ api/app/utils/app_config.py
@@ -1,4 +1,39 @@
-class AppConfig:
+import json
 
-    def __init__(self):
-        pass
+
+class AppConfig:
+    _instance = None
+
+    name: str = "VEGA Historical Object Detection and Segmentation API"
+    version: str = "1.0.0"
+    host: str = "127.0.0.1"
+    port: int = 8000
+    yolo_v8_digits_source: str = "../resources/weights/yolo_v8_digits.pt"
+    yolo_v8_glyphs_source: str = "../resources/weights/yolo_v8_glyphs.pt"
+    yolo_v8_explorer_source: str = "../resources/weights/yolo_v8_explorer.pt"
+    rt_detr_digits_source: str = "../resources/weights/rt_detr_digits.pt"
+    rt_detr_glyphs_source: str = "../resources/weights/rt_detr_glyphs.pt"
+    fast_sam_source: str = "../resources/weights/FastSAM-s.pt"
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+            try:
+                with open("../config/config.json") as json_config_file:
+                    config_data = json.load(json_config_file)
+                    cls.name = config_data["name"]
+                    cls.version = config_data["version"]
+                    cls.host = config_data["host"]
+                    cls.port = config_data["port"]
+                    cls.yolo_v8_digits_source = config_data["yolo_v8_digits_source"]
+                    cls.yolo_v8_glyphs_source = config_data["yolo_v8_glyphs_source"]
+                    cls.yolo_v8_explorer_source = config_data["yolo_v8_explorer_source"]
+                    cls.rt_detr_digits_source = config_data["rt_detr_digits_source"]
+                    cls.rt_detr_glyphs_source = config_data["rt_detr_glyphs_source"]
+                    cls.fast_sam_source = config_data["fast_sam_source"]
+            except (FileNotFoundError, KeyError, ValueError, TypeError) as e:
+                print(e)
+                pass
+
+        return cls._instance
