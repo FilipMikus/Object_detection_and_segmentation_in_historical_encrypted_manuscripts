5a5d5c8c1bd7f1f80d0c6a1a8a71f544dff495de
Filip Mikus <xmikusf@stuba.sk>
2024-03-23 11:11:56 +0100

    fix: Project structure clean

--- api/app/router/data_converter_router.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-import http
-
-from fastapi import APIRouter, File, UploadFile, Form, Response
-import io
-
-# TODO
-data_converter_router = APIRouter(prefix="/data/converter", tags=["data_converter"])
--- api/app/service/data_converter_service.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-
-
-class DataConverterService(ABC):
-    # TODO
-
-    def __init__(self):
-        pass
